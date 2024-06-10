10b3c1b9b16d0be280283fb60aabd5d7ac3ca7fb
Ryan <ryansaftje123@gmail.com>
2024-06-10 23:25:32 +0700

    Mengganti API request client dari local menjadi server

--- FrontEnd/DevKiller/app/src/main/java/com/ryansatj/devkill_android/request/UtilsApi.java
+++ FrontEnd/DevKiller/app/src/main/java/com/ryansatj/devkill_android/request/UtilsApi.java
@@ -3,7 +3,7 @@ package com.ryansatj.devkill_android.request;
 import android.content.Context;
 
 public class UtilsApi {
-    public static final String BASE_URL_API = "http://10.0.2.2:4001/";
+    public static final String BASE_URL_API = "https://devkill-pm-system.vercel.app/";
 
     public static BaseApiService getApiService(Context context) {
         return RetrofitClient.getClient(BASE_URL_API, context).create(BaseApiService.class);
